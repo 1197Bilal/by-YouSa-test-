1e2dddfee2b5e33357ac75d87f39679fe37193d1
1197Bilal <elhbilal97@gmail.com>
2026-01-20 12:32:53 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("by YouSa | Premium Modest Fashion loaded");
+
+// Simple interaction placeholder
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.nav-toggle');
+    if (navToggle) {
+        navToggle.addEventListener('click', () => {
+            console.log("Toggle menu");
+            // Implement simple menu slide later
+        });
+    }
+});
